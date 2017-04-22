12d6582a54adb1122cb5fcd1f41ffc9b9d00a2a8
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-04-22 19:48:55 +0200

    Some more Random extensions

--- korma/src/com/soywiz/korma/random/RandomExt.kt
+++ korma/src/com/soywiz/korma/random/RandomExt.kt
@@ -6,3 +6,8 @@ fun <T> List<T>.random(random: Random = Random()): T {
 	if (this.isEmpty()) throw IllegalArgumentException("Empty list")
 	return this[random.nextInt(this.size)]
 }
+
+operator fun Random.get(min: Double, max: Double): Double = min + nextDouble() * (max - min)
+operator fun Random.get(min: Int, max: Int): Int = min + nextInt(max - min)
+operator fun Random.get(range: IntRange): Int = range.start + this.nextInt(range.endInclusive - range.start + 1)
+operator fun Random.get(range: LongRange): Long = range.start + this.nextLong() % (range.endInclusive - range.start + 1)
