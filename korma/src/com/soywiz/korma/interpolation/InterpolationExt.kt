b83dc2ba77a2bbfbb752bcea2064654984b34e43
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-05-06 20:05:38 +0200

    Some interpolate work

--- korma/src/com/soywiz/korma/interpolation/InterpolationExt.kt
+++ korma/src/com/soywiz/korma/interpolation/InterpolationExt.kt
@@ -1,5 +1,7 @@
 package com.soywiz.korma.interpolation
 
+fun Double.interpolate(l: Float, r: Float): Float = (l + (r - l) * this).toFloat()
 fun Double.interpolate(l: Double, r: Double): Double = l + (r - l) * this
 fun Double.interpolate(l: Int, r: Int): Int = (l + (r - l) * this).toInt()
+fun Double.interpolate(l: Long, r: Long): Long = (l + (r - l) * this).toLong()
 fun <T : Interpolable<T>> Double.interpolate(l: T, r: T): T = l.interpolateWith(r, this)
