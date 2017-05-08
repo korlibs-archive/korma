41ec605079275c7178657a268a52f2c2aa56806f
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-05-08 19:34:40 +0200

    Added some random utilities

--- korma/src/com/soywiz/korma/interpolation/InterpolationExt.kt
+++ korma/src/com/soywiz/korma/interpolation/InterpolationExt.kt
@@ -5,3 +5,4 @@ fun Double.interpolate(l: Double, r: Double): Double = l + (r - l) * this
 fun Double.interpolate(l: Int, r: Int): Int = (l + (r - l) * this).toInt()
 fun Double.interpolate(l: Long, r: Long): Long = (l + (r - l) * this).toLong()
 fun <T : Interpolable<T>> Double.interpolate(l: T, r: T): T = l.interpolateWith(r, this)
+fun <T> Double.interpolate(l: Interpolable<T>, r: Interpolable<T>): T = l.interpolateWith(r as T, this)
---
+++ korma/test/com/soywiz/korma/random/RandomExtKtTest.kt
@@ -0,0 +1,37 @@
+package com.soywiz.korma.random
+
+import org.junit.Assert
+import org.junit.Test
+import java.util.*
+
+class RandomExtKtTest {
+	//class TestRandom(val values: List<Int>) : Random() {
+	//	var index = 0
+	//	//override fun next(bits: Int): Int = values.getCyclic(index++) and ((1L shl bits) - 1).toInt()
+	//	override fun next(bits: Int): Int {
+	//		return values.getCyclic(index++)
+	//	}
+	//}
+
+	@Test
+	fun weighted() {
+		val random = Random(0L)
+		val weighted = mapOf("a" to 1, "b" to 1)
+		random.nextInt()
+		Assert.assertEquals("b", random[weighted])
+		Assert.assertEquals("b", random[weighted])
+		Assert.assertEquals("a", random[weighted])
+		Assert.assertEquals("a", random[weighted])
+	}
+
+	@Test
+	fun weighted2() {
+		val weighted = mapOf("a" to 1, "b" to 2)
+		val random = Random(0L)
+		Assert.assertEquals("b", random[weighted])
+		Assert.assertEquals("a", random[weighted])
+		Assert.assertEquals("a", random[weighted])
+		Assert.assertEquals("b", random[weighted])
+		Assert.assertEquals("b", random[weighted])
+	}
+}
