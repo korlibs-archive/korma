23746bf21edee737c64c261c8c5a6da8618ae738
soywiz <soywiz@gmail.com>
2017-10-04 23:56:41 +0200

    Implement binarySearch

--- korma-common/src/main/kotlin/com/soywiz/korma/buffer/ArrayExt.kt
+++ korma-common/src/main/kotlin/com/soywiz/korma/buffer/ArrayExt.kt
@@ -8,5 +8,40 @@ fun DoubleArray.copyTo(srcPos: Int, dst: DoubleArray, dstPos: Int, count: Int) {
 	for (n in 0 until count) dst[dstPos + n] = this[srcPos + n]
 }
 
-fun IntArray.binarySearch(v: Int, fromIndex: Int, toIndex: Int): Int = TODO()
-fun DoubleArray.binarySearch(v: Double, fromIndex: Int, toIndex: Int): Int = TODO()
+fun IntArray.binarySearch(v: Int, fromIndex: Int, toIndex: Int): Int {
+	var low = fromIndex
+	var high = toIndex - 1
+
+	while (low <= high) {
+		val mid = (low + high) / 2
+		val mval = this[mid]
+
+		if (mval < v) {
+			low = mid + 1
+		} else if (mval > v) {
+			high = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return -low - 1
+}
+
+fun DoubleArray.binarySearch(v: Double, fromIndex: Int, toIndex: Int): Int {
+	var low = fromIndex
+	var high = toIndex - 1
+
+	while (low <= high) {
+		val mid = (low + high) / 2
+		val mval = this[mid]
+
+		if (mval < v) {
+			low = mid + 1
+		} else if (mval > v) {
+			high = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return -low - 1
+}
