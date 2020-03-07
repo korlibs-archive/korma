047adfd73305b0479a116b1c86316507acb1d6e8
soywiz <soywiz@gmail.com>
2020-03-07 17:42:25 +0100

    Add failing test cases

--- korma-shape/src/commonTest/kotlin/com/soywiz/korma/geom/triangle/triangulate/TriangulateTest.kt
+++ korma-shape/src/commonTest/kotlin/com/soywiz/korma/geom/triangle/triangulate/TriangulateTest.kt
@@ -1,5 +1,6 @@
 package com.soywiz.korma.geom.triangle.triangulate
 
+import com.soywiz.korma.geom.*
 import com.soywiz.korma.geom.vector.*
 import com.soywiz.korma.triangle.triangulate.*
 import kotlin.test.*
@@ -13,6 +14,44 @@ class TriangulateTest {
                 rect(0, 0, 100, 100)
                 rect(300, 0, 100, 100)
             }.triangulate().toString()
+        )
+    }
+
+    @Test
+    fun test2() {
+        val points = listOf(
+            Point(3, 10),
+            Point(1, 5),
+            Point(3, 1),
+            Point(4, 0),
+            Point(6, 0)
+        )
+
+        assertEquals(
+            "...",
+            points.triangulate().toString()
+        )
+    }
+
+    @Test
+    fun test3() {
+        val points = listOf(
+            Point(-371.182, 307.381),
+            Point(-365.909, 310.721),
+            Point(-369.425, 318.455),
+            Point(-373.468, 317.048),
+            Point(-375.401, 315.818),
+            Point(-376.28, 314.588),
+            Point(-376.28, 313.357),
+            Point(-375.753, 311.599),
+            Point(-373.643, 307.733),
+            Point(-372.764, 307.029),
+            Point(-372.061, 307.029)
+        )
+
+        assertEquals(
+            "...",
+            points.triangulate().toString()
         )
     }
 }
