a2bd69393b3c19748791836302ed6c8f2b3dc061
Sergei Bulgakov <servbul@yandex.ru>
2021-01-02 17:19:06 +0300

    Add corners properties for IRectangleInt
    1. Add simple corners properties (same as for IRectangle: topLeft, topRight, bottomLeft, bottomRight)
    2. Add inner corners coordinates (`x2 = right - 1`, `y2 = bottom - 1`)
    3. Add inner corners properties (topLeft2, topRight2, bottomLeft2, bottomRight2)

--- korma/src/commonMain/kotlin/com/soywiz/korma/geom/Rectangle.kt
+++ korma/src/commonMain/kotlin/com/soywiz/korma/geom/Rectangle.kt
@@ -306,6 +306,19 @@ inline fun IRectangleInt.anchor(ax: Number, ay: Number): IPointInt = anchor(ax.t
 
 val IRectangleInt.center get() = anchor(0.5, 0.5)
 
+val IRectangleInt.topLeft get() = PointInt(left, top)
+val IRectangleInt.topRight get() = PointInt(right, top)
+val IRectangleInt.bottomLeft get() = PointInt(left, bottom)
+val IRectangleInt.bottomRight get() = PointInt(right, bottom)
+
+val IRectangleInt.x2: Int get() = x + width - 1
+val IRectangleInt.y2: Int get() = y + height - 1
+
+val IRectangleInt.topLeft2 get() = PointInt(x, y)
+val IRectangleInt.topRight2 get() = PointInt(x2, y)
+val IRectangleInt.bottomLeft2 get() = PointInt(x, y2)
+val IRectangleInt.bottomRight2 get() = PointInt(x2, y2)
+
 ///////////////////////////
 
 fun Iterable<Rectangle>.bounds(target: Rectangle = Rectangle()): Rectangle {
